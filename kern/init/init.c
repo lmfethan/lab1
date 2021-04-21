2663b2ab3b37c1857cf77c9606fa9da2ee238f3a
lmfethan <39012918@qq.com>
2021-04-21 12:07:53 +0800

    m

--- kern/init/init.c
+++ kern/init/init.c
@@ -8,9 +8,13 @@
 #include <clock.h>
 #include <intr.h>
 #include <pmm.h>
-#include <kmonitor.h>
+#include <vmm.h>
+#include <ide.h>
+#include <swap.h>
+#include <proc.h>
+
 int kern_init(void) __attribute__((noreturn));
-void grade_backtrace(void);
+
 static void lab1_switch_test(void);
 
 int
@@ -32,15 +36,20 @@ kern_init(void) {
     pic_init();                 // init interrupt controller
     idt_init();                 // init interrupt descriptor table
 
+    vmm_init();                 // init virtual memory management
+    proc_init();                // init process table
+    
+    ide_init();                 // init ide devices
+    swap_init();                // init swap
+
     clock_init();               // init clock interrupt
     intr_enable();              // enable irq interrupt
 
     //LAB1: CAHLLENGE 1 If you try to do it, uncomment lab1_switch_test()
     // user/kernel mode switch test
     //lab1_switch_test();
-
-    /* do nothing */
-    while (1);
+    
+    cpu_idle();                 // run idle process
 }
 
 void __attribute__((noinline))
